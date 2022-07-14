f0a9dc4d3f5392a73051e1b02c041a3c9934a397
Ali Aliyev <106176887+im-Aliga@users.noreply.github.com>
2022-07-14 19:50:32 +0400

    employe model is completed

---
+++ person-managment/person-managment/DataBase/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace person_managment.DataBase.Models
+{
+    internal class Employee
+    {
+        private static int _idcounter = 1;
+        private int Id { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public string FatherName { get; set; }
+        public string FinCode { get; set; }
+        public string Email { get; set; }
+
+        public Employee()
+        {
+
+        }
+
+    }
+}
