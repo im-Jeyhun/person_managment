ff7e197f63cec9c057c06ed04a324f5d480d072a
Ali Aliyev <106176887+im-Aliga@users.noreply.github.com>
2022-07-14 21:15:57 +0400

    Adress reposity update and completed

--- person-managment/person-managment/DataBase/Repository/AdressReposiytory.cs
+++ person-managment/person-managment/DataBase/Repository/AdressReposiytory.cs
@@ -9,12 +9,13 @@ namespace person_managment.DataBase.Repository
 {
     internal class AdressReposiytory
     {
-        private static List<Adress> Adress = new List<Adress>();
+        private static List<Adress> _adresses = new List<Adress>();
+        
 
         public static Adress Add(string name)
         {
             Adress newAdress = new Adress(name);
-            Adress.Add(newAdress);
+            _adresses.Add(newAdress);
             return newAdress;
 
             
@@ -22,25 +23,50 @@ namespace person_managment.DataBase.Repository
         public static void Remove(int targetId)
         {
 
-            foreach (Adress adress in Adress)
+            foreach (Adress adress in _adresses)
             {
                 if (adress.Id == targetId)
                 {
-                    Adress.Remove(adress);
+                    _adresses.Remove(adress);
                 }
             }
             
         }
         public static void RemoveAt(int targetId)
         {
-            for (int i = 0; i < Adress.Count; i++)
+            for (int i = 0; i < _adresses.Count; i++)
             {
-                if (Adress[i].Id == targetId)
+                if (_adresses[i].Id == targetId)
                 {
-                    Adress.RemoveAt(i);
+                    _adresses.RemoveAt(i);
                 }
             }
             
+        }
+        public static Adress Update( int id ,string name)
+        {
+            Adress adress = GetById(id);
+
+            adress.Name = name;
+
+
+            return adress;
+        }
+        public static Adress GetById(int id)
+        {
+            for (int i = 0; i < _adresses.Count; i++)
+            {
+                if (_adresses[i].Id == id)
+                {
+                    return _adresses[i];
+                }
+            }
+
+            return null;
+        }
+        public static List<Adress> GetAll()
+        {
+            return _adresses;
         }
     }
 }
