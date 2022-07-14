77f8d9f40854c11b26c1c18d9b3d143265ffd3b7
Ali Aliyev <106176887+im-Aliga@users.noreply.github.com>
2022-07-14 20:18:32 +0400

    Adress model completed

---
+++ person-managment/person-managment/DataBase/Models/Adress.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace person_managment.DataBase.Models
+{
+    internal class Adress
+    {
+        public string Name { get; set; }
+        public  int ID { get; }
+        private static int _idcounter = 1;
+        public Adress(string name)
+        {
+            Name = name;
+            ID = _idcounter;
+            _idcounter++;
+        }
+    }
+}
