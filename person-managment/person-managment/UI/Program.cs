936ccb90492f3a17e1a695d62b88fa91bee08883
Ceyhun <contact.ceyhun@gmail.com>
2022-07-14 19:24:31 +0400

    inital structor

---
+++ person-managment/person-managment/UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace person_managment.UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
