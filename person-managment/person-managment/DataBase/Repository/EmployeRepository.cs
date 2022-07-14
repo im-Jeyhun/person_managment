8e1b5fa594d7bd10412fb2a47c4815b8b86fd931
Ceyhun <contact.ceyhun@gmail.com>
2022-07-14 20:11:58 +0400

    Add method added to EmployeeRepository

---
+++ person-managment/person-managment/DataBase/Repository/EmployeRepository.cs
@@ -0,0 +1,25 @@
+﻿using person_managment.DataBase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace person_managment.DataBase.Repository
+{
+    internal class EmployeRepository
+    {
+       private static List<Employee> _employees = new List<Employee>();
+
+        public static Employee Add(string name , string lastName , string fatherName , string FINN , string email)
+        {
+            Employee newemployee = new Employee(name , lastName, fatherName, FINN , email);
+
+            _employees.Add(newemployee);
+
+            return newemployee;
+        }
+
+
+    }
+}
