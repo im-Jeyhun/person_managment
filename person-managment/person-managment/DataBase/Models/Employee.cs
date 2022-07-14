7706fc3a7fb0fccf28f331b8f54349fbd39bdbce
Ali Aliyev <106176887+im-Aliga@users.noreply.github.com>
2022-07-14 19:52:52 +0400

    completed employee models again

--- person-managment/person-managment/DataBase/Models/Employee.cs
+++ person-managment/person-managment/DataBase/Models/Employee.cs
@@ -9,16 +9,22 @@ namespace person_managment.DataBase.Models
     internal class Employee
     {
         private static int _idcounter = 1;
-        private int Id { get; set; }
+        public int Id { get; }
         public string Name { get; set; }
         public string Lastname { get; set; }
         public string FatherName { get; set; }
         public string FinCode { get; set; }
         public string Email { get; set; }
 
-        public Employee()
+        public Employee(string name,string latsname,string fathername,string fincode,string email)
         {
-
+            Id = _idcounter;
+            _idcounter++;
+            Name = name;
+            Lastname = latsname;
+            FatherName = fathername;
+            FinCode = fincode;
+            Email = email;
         }
 
     }
