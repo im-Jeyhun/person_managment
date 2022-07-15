14dde50fd34adbfd44ed9c96b4c3a206635a4bb4
Ceyhun <contact.ceyhun@gmail.com>
2022-07-15 18:01:06 +0400

    Register method added to Authentication

---
+++ person-managment/person-managment/ApplicationLogic/Authentication.cs
@@ -0,0 +1,55 @@
+﻿using person_managment.ApplicationLogic.Validation;
+using person_managment.DataBase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace person_managment.ApplicationLogic
+{
+    internal class Authentication
+    {
+        public static void Register()
+        {
+            string firstName = GetFirstName();
+            string lastName = GetLastName();
+
+        }
+
+        public static string GetFirstName ()
+        {
+            Console.Write("Pls enter name : ");
+            string firstName = Console.ReadLine();
+            while (!EmployeeValidation.IsValidFirstName(firstName))
+            {
+                Console.Write("Pls enter name again");
+                firstName = Console.ReadLine();
+            }
+            return firstName;
+        } 
+
+        public static string GetLastName()
+        {
+            Console.Write("Pls enter surname : ");
+            string lastName = Console.ReadLine();
+            while (!EmployeeValidation.IsValidLastName(lastName))
+            {
+                Console.Write("Pls enter surname again : ");
+                 lastName = Console.ReadLine();
+            }
+            return lastName;
+        }
+        public static string GetFatherName()
+        {
+            Console.Write("Pls enter father name : ");
+            string fatherName = Console.ReadLine();
+            while (!EmployeeValidation.IsValidFatherName(fatherName))
+            {
+                Console.Write("Pls enter father name again : ");
+                fatherName = Console.ReadLine();
+
+            }
+        }
+    }
+}
--- person-managment/person-managment/UI/Program.cs
+++ person-managment/person-managment/UI/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using person_managment.DataBase.Models;
+using person_managment.DataBase.Repository;
+using System;
+using System.Collections.Generic;
 
 namespace person_managment.UI
 {
@@ -6,7 +9,50 @@ namespace person_managment.UI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("/show-persons");
+            Console.WriteLine("/add-person");
+            Console.WriteLine("/remove-person");
+            Console.WriteLine("/update-person");
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if(command == "/show-persons")
+                {
+                
+                    List<Employee> employes = EmployeRepository.GetAll();
+                    foreach (Employee employe in employes)
+                    {
+                        Console.WriteLine(employe);
+                    }
+                    
+                }
+                else if (command == "/add-person")
+                {
+                    Console.WriteLine("Enter name");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Enter surname");
+                    string lastName = Console.ReadLine();
+                    Console.WriteLine("Enter Father name");
+                    string fatherName = Console.ReadLine();
+                    Console.WriteLine("Enter Finn");
+                    string finn = Console.ReadLine();
+                    Console.WriteLine("Enter email");
+                    string email = Console.ReadLine();
+
+                    EmployeRepository.Add(name, lastName, fatherName, finn, email);
+                }
+                else if (command== "/remove-person")
+                {
+                    Console.WriteLine("Pls enter finn which u want to delete");
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    EmployeRepository.Remove(id);
+                }
+
+            }
+
+
+
 
         }
     }
